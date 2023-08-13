d2aca34d7de0e5895ff1af0cd22a299c63998186
ramyalbanna <ramyalbanna12@gmail.com>
2023-08-13 07:17:26 +0300

    remove unused imports

--- src/app/modules/website/components/header/header.component.ts
+++ src/app/modules/website/components/header/header.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnDestroy, OnInit, inject } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Subscription } from "rxjs";
 import { Router } from "@angular/router";
-import { ToastrService } from "ngx-toastr";
 import { CartsService } from "src/app/core/services/carts.service";
 import { CartModel } from "src/app/core/models/carts.model";
 
@@ -11,7 +9,7 @@ import { CartModel } from "src/app/core/models/carts.model";
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   private readonly _cartsService = inject(CartsService);
   private readonly _router = inject(Router);
 
@@ -23,12 +21,18 @@ export class HeaderComponent implements OnInit {
   }
 
   setCartData = () => {
-    this._cartsService.cartDataSubject$.subscribe({
-      next: (cart: CartModel) => (this.cartData = cart),
-    });
+    this.subscription.add(
+      this._cartsService.cartDataSubject$.subscribe({
+        next: (cart: CartModel) => (this.cartData = cart),
+      })
+    );
   };
 
   onSignIn = () => {
     this._router.navigate(["auth"]);
   };
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
